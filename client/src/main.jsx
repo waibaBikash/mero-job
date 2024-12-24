ecb57bf6dbf58dbaf3ae5ce3b85474eb4b85e51e
waibaBikash <kavrelibikash@gmail.com>
2024-12-24 21:29:01 +1100

    create context and use

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContxt.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-       <App />
+  <AppContextProvider>
+     <App />
+  </AppContextProvider>     
   </BrowserRouter>,
     
   
