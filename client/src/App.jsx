687cea192beeac23d51f7f543e42365d4a9fbd4c
waibaBikash <kavrelibikash@gmail.com>
2024-12-24 21:21:36 +1100

    create a pages and api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Application from './pages/Application'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+         <Route path='/' element = {<Home/>}/>
+         <Route path='/apply-job/:id' element = {<ApplyJob/>}/>
+         <Route path='/applications' element = {<Application/>}/>
+      </Routes>
+    </div>
   )
 }
 
