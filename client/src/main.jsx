50f080da5c35d5e28dce5856f76b7c693bf56c35
waibaBikash <kavrelibikash@gmail.com>
2024-12-25 22:01:37 +1100

    login functianality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey='{PUBLISHABLE_KEY} afterSignOutUrl="/"'>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
   <BrowserRouter>
   <AppContextProvider>
      <App />
