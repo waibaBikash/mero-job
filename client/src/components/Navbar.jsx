0e6a04b49d0410a79dde5102c50fc7fdb48133cb
waibaBikash <kavrelibikash@gmail.com>
2024-12-25 22:02:11 +1100

    save change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import React from 'react';
+import { assets } from '../assets/assets';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -30,4 +30,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
