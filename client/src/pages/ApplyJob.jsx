d36a1b59a438c50b464e23533b33eb9738605d6e
waibaBikash <kavrelibikash@gmail.com>
2024-12-28 23:16:45 +1100

    changes made

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const ApplyJob = () => {
   return (
@@ -6,4 +6,4 @@ const ApplyJob = () => {
   )
 }
 
-export default ApplyJob
+export default ApplyJob;
