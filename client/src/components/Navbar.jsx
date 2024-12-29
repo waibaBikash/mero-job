862e8d7a86fd888247c92894c7b23b46283a4627
waibaBikash <kavrelibikash@gmail.com>
2024-12-29 21:09:09 +1100

    commit change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = () => {
           <img src={assets.logo} alt="logo" />
           {
             user
-            ? <div>
-              <Link to={"/applications"}>Applied Jobs</Link>
+            ? <div className='flex items-center gap-3'>
+              <Link to={'/applications'}>Applied Jobs</Link>
               <p>|</p>
               <p>Hi, {user.firstName+" "+user.lastName}</p>
               <UserButton/>
