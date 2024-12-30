5e3d6a3196bd3b0ba33e30aae6b4639620aa0578
waibaBikash <kavrelibikash@gmail.com>
2024-12-30 21:20:22 +1100

    search functionality

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContxt'
 
 const Hero = () => {
+  const {setSearchFilter, setIsSearched} = useContext(AppContext)
+
+  const titleRef = useRef(null)
+  const locationRef = useRef(null)
+  
+  const onSearch = () => {
+    setSearchFilter({
+      title:titleRef.current.value,
+      location:locationRef.current.value
+    })
+    setIsSearched(true)
+    
+  }
   return (
     <div className='container 2xl:px-20 mx-auto my-10 '>
        <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
@@ -10,13 +24,13 @@ const Hero = () => {
           <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto'>
              <div className='flex items-center'>
                <img className='h-4 sm:h-5' src={assets.search_icon} alt="" />
-               <input type="text" placeholder='Search for jobs' className='max-sm:text-sm p-2 rounded outline-none w-full' />
+               <input type="text" placeholder='Search for jobs' className='max-sm:text-sm p-2 rounded outline-none w-full' ref={titleRef} />
              </div>
              <div className='flex items-center'>
                <img className='h-4 sm:h-5' src={assets.location_icon} alt="" />
-               <input type="text" placeholder='Location' className='max-sm:text-sm p-2 rounded outline-none w-full' />
+               <input type="text" placeholder='Location' className='max-sm:text-sm p-2 rounded outline-none w-full' ref={locationRef}/>
              </div>
-              <button className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
+              <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1'>Search</button>
           </div>
        </div>
          <div className='border border-gray-300 shadow-md mt-5 p-6 rounded-md flex'>
