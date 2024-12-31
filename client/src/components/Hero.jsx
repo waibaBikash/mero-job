2c2ad449429fa1552548da74aaf636a17da0a899
waibaBikash <kavrelibikash@gmail.com>
2024-12-31 22:13:57 +1100

    commit change

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -19,8 +19,12 @@ const Hero = () => {
   }
   return (
     <div className='container 2xl:px-20 mx-auto my-10 '>
-       <div className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
-         <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Over 10,000+ jobs to apply</h2>
+       <div 
+         className='bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl'>
+          <h2 
+            className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>
+            Over 10,000+ jobs to apply
+            </h2>
          <p className='mb-8 max-w-xl mx-auto text-sm font-light px-5'>Your Next Big Career Move Starts Rights Here - Explore the Best job Oppertunites and Take Step Towards Your Future! </p>
           <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto'>
              <div className='flex items-center'>
