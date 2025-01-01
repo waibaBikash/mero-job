ecd2f7c23854ae43a6f79204ae6e0b10240612c0
waibaBikash <kavrelibikash@gmail.com>
2025-01-01 22:55:57 +1100

    aplly tailwind css

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -6,23 +6,23 @@ const JobListing = () => {
 
   const {isSearched, searchFilter, setSearchFilter} = useContext(AppContext)
   return (
-    <div>
+    <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
       {/* Sidebar */}
-      <div>
+      <div className='w-full lg:w-1/4 bg-white px-4 '>
          {/* Search Filter from hero components */}
           {
             isSearched && ( searchFilter.title !== "" || searchFilter.location !== "") && (
               <>
-              <h3>Current Search</h3>
-                 <div>
+              <h3 className='font-medium text-lg mb-4'>Current Search</h3>
+                 <div className='mb-4 text-gray-600'>
                    {searchFilter.title && (
-                     <span>
+                     <span className='inline-flex items-center gap-2.5 bg-blue-50 border border-blue-200 px-4 py-1.5 rounded '>
                       {searchFilter.title}
                       <img onClick={ e => setSearchFilter(prev => ({...prev, title:""})) } className='cursor-pointer' src={assets.cross_icon} alt="" />
                      </span>
                    )}
                    {searchFilter.location && (
-                     <span>
+                     <span className=' ml-2 inline-flex items-center gap-2.5 bg-red-50 border border-red-200 px-4 py-1.5 rounded'>
                         {searchFilter.location}
                         <img onClick={ e => setSearchFilter(prev => ({...prev, location:""})) } className='cursor-pointer' src={assets.cross_icon} alt="" />
                      </span>
