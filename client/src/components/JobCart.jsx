42e589d6ac00d02e83f0f69044999b22b94ce049
waibaBikash <kavrelibikash@gmail.com>
2025-01-04 23:52:32 +1100

    commit change

--- client/src/components/JobCart.jsx
+++ client/src/components/JobCart.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from 'react';
+import { assets } from '../assets/assets';
 
 const JobCart = ({ job }) => {
   return (
