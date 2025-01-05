42f48b89f408b85ad3361607caa6c02c3a484064
waibaBikash <kavrelibikash@gmail.com>
2025-01-05 21:27:46 +1100

    save change

--- client/src/context/AppContxt.jsx
+++ client/src/context/AppContxt.jsx
@@ -4,7 +4,8 @@ import { jobsData } from "../assets/assets";
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-   const [searchFilter, setSearchFilter] = useState({
+   const [searchFilter, 
+      setSearchFilter] = useState({
       title:'',
       location:''
    });
